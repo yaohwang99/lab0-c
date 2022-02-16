cfdf612d0c5a4cb896747ff874d5f86d8137aeeb
Yao Hwang <yaohwang99@gmail.com>
2022-02-16 15:22:22 +0800

    Complete q_size function from tutorial

--- queue.c
+++ queue.c
@@ -91,8 +91,17 @@ void q_release_element(element_t *e)
  */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+
+    int len = 0;
+    struct list_head *li;
+
+    list_for_each (li, head)
+        len++;
+    return len;
 }
+
 
 /*
  * Delete the middle node in list.
