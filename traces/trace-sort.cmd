ce58bca1ccb521d76e784b8e2589c896299e4765
Yao Hwang <yaohwang99@gmail.com>
2022-02-26 13:13:43 +0800

    Compare speed of both sort method

---
+++ traces/trace-sort.cmd
@@ -0,0 +1,35 @@
+# Test performance of sort and linux sort with random orders
+option fail 0
+option malloc 0
+new
+ih RAND 500000
+time sort 0
+new
+ih RAND 500000
+time sort 0
+new
+ih RAND 500000
+time sort 0
+new
+ih RAND 500000
+time sort 0
+new
+ih RAND 500000
+time sort 0
+new
+ih RAND 500000
+time sort
+new
+ih RAND 500000
+time sort
+new
+ih RAND 500000
+time sort
+new
+ih RAND 500000
+time sort
+new
+ih RAND 500000
+time sort
+# Exit program
+quit
